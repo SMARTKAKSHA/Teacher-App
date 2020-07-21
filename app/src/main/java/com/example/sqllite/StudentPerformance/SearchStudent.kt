bc5581522ebbe1941a36a3237d3ea2f69d79f0ce
Divyanshu Gupta <44648798+divyanshuguptaososofficial@users.noreply.github.com>
2020-07-21 12:27:04 +0530

    Search Student cohort

---
+++ app/src/main/java/com/example/sqllite/StudentPerformance/SearchStudent.kt
@@ -0,0 +1,11 @@
+package com.example.sqllite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchStudent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_student)
+    }
+}
